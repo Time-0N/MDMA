431af26bfc2ae6c0cfdbf6b1287d30f43a7a2d7e
Timeon <time.onpsoff@gmail.com>
2025-05-21 11:26:48 +0200

    User data now appears on the user-home. Angular components will now use scss instead of css

--- frontend/src/app/components/pages/user-home/user-home/user-home.component.ts
+++ frontend/src/app/components/pages/user-home/user-home/user-home.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {User} from '../../../../generated';
+import {selectUser} from '../../../../store/user-store/user-store.selector';
+import {AsyncPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-user-home',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    NgIf
+  ],
   templateUrl: './user-home.component.html',
-  styleUrl: './user-home.component.css'
+  styleUrl: './user-home.component.scss'
 })
 export class UserHomeComponent {
-
+  user$: Observable<User | null>;
+  constructor(
+    private readonly store: Store
+  ) {
+    this.user$ = this.store.select(selectUser);
+  }
 }
